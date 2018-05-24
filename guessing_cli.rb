3253002b1cfd17a346a055d99b8d861e8d613e8c
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-24 18:28:58 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -20,4 +20,20 @@ def run_guessing_game
   # end
   # puts "Goodbye!"
   
+  loop do
++      puts "Guess a number between 1 and 6."
++      guess = gets.chomp
++      computer_number = rand(1..6)
++      if guess == "exit"
++        puts "Goodbye!"
++        break
++      elsif guess.to_i == computer_number
++          puts "You guessed the correct number!"
++        elsif guess.to_i != computer_number
++          puts "The computer guessed #{computer_number}."
++        else
++          puts "Invalid input"
++        end
++      end
+  
 end
