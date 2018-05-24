2ee2de5f9e4eac21a9c9ab9221b0ec10e241e23b
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-24 18:31:48 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,38 +2,38 @@
 
 def run_guessing_game
   
-  # loop do 
-  #   puts "Guess a number between 1 and 6."
-  #   user = gets.chomp
-  #   comp = (1..6).to_a.sample
-    
-  #   if user == "exit"
-  #     break
-  #   elsif user.to_i == comp
-  #     puts "You guessed the correct number!"
-  #     elsif user.to_i != comp
-  #       puts "The computer guessed #{comp}."
-  #     else
-  #       "Invalid. Try again."
-  #   end
-    
-  # end
-  # puts "Goodbye!"
-  
-  loop do
+  loop do 
     puts "Guess a number between 1 and 6."
     user = gets.chomp
     comp = (1..6).to_a.sample
-    if guess == "exit"
-      puts "Goodbye!"
+    
+    if user == "exit"
       break
     elsif user.to_i == comp
       puts "You guessed the correct number!"
-    elsif user.to_i != comp
-      puts "The computer guessed #{comp}."
-    else
-      puts "Invalid input"
+      elsif user.to_i != comp
+        puts "The computer guessed #{comp}."
+      else
+        "Invalid. Try again."
     end
+    
   end
+  puts "Goodbye!"
+  
+  # loop do
+  #   puts "Guess a number between 1 and 6."
+  #   user = gets.chomp
+  #   comp = (1..6).to_a.sample
+  #   if guess == "exit"
+  #     puts "Goodbye!"
+  #     break
+  #   elsif user.to_i == comp
+  #     puts "You guessed the correct number!"
+  #   elsif user.to_i != comp
+  #     puts "The computer guessed #{comp}."
+  #   else
+  #     puts "Invalid input"
+  #   end
+  # end
   
 end
